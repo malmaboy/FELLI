643754251c0cb958cedb3d9884a6556ef1159727
francismsc <60035994+francismsc@users.noreply.github.com>
2020-05-16 15:59:28 +0100

    Adicionados mais comentários

--- State.cs
+++ State.cs
@@ -1,12 +1,15 @@
 namespace Felli
 {
     /// <summary>
-    /// As diferentes peças do tabuleiro.
+    /// Os diferentes tipos de estados das peças do tabuleiro.
     /// </summary>
     public enum State
     {
+        /// Define o estado como vazio
         Empty,
+        /// Define o estado como peça preta
         B, // Peças pretas
+        /// Define o estado como peça branca
         W // Peças Brancas
         
 
